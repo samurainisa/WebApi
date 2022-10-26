c7b80531a5989777cab8b9701c5cb376972312f1
samurainisa <aressamurai@mail.ru>
2022-10-26 12:39:34 +0700

    AddTreeNode

---
+++ ClientWindowsForms/ClubStructure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWindowsForms
+{
+    public class ClubStructure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ClubStructure[] Childs { get; set; }
+    }
+}
--- ClientWindowsForms/Form1.cs
+++ ClientWindowsForms/Form1.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ClientWindowsForms
 {
@@ -35,18 +38,66 @@ namespace ClientWindowsForms
 
                 // получаем ответ
                 HttpResponseMessage response = await client.GetAsync("api/Clubs");
+
                 if (response.IsSuccessStatusCode)
                 {
                     // читаем ответ
                     string content = await response.Content.ReadAsStringAsync();
+                    //добавить раскрывающийся заголовок "Клубы"
+                    //добавить к корню последовательность клубов по типу, клуб 1, клуб 2, клуб 3, и при нажатии на клуб открывать название клуба и id
 
-                    foreach (var item in content)
-                    {
-                        listBox1.Items.Add(item);
-                    }
+                    TreeNode rootNode = new TreeNode("База данных");
+                    TreeNode tablesNode = new TreeNode("Таблицы");
+                    TreeNode clubNode = new TreeNode("Клубы");
+
+                    rootNode.Nodes.Add(tablesNode);
+
+                    tablesNode.Nodes.Add(clubNode);
+
+                    clubNode.Nodes.AddRange(ClubStructuresToTreeNodes(DeserializeClubs(content)));
+                    treeView1.Nodes.Add(rootNode);
+
+
+                    // treeView1.Nodes.AddRange(StructuresToTreeNodes(DeserializeData(content)));
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка подключения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+        }
 
+        public ClubStructure[] DeserializeClubs(string data)
+        {
+            //десериализация данных
+            var json = JToken.Parse(data);
+            var result = json.ToObject<ClubStructure[]>();
+        
+            return result;
+        }
+
+
+
+        public TreeNode[] ClubStructuresToTreeNodes(ClubStructure[] structures)
+        {
+            //преобразование данных в дерево
+            var result = new List<TreeNode>();
+            foreach (var structure in structures)
+            {
+                var nodeName = new TreeNode(structure.Name);
+                var nodeId = new TreeNode(structure.Id.ToString());
+                nodeName.Tag = structure;
+                nodeId.Tag = structure;
+                var commonNode = new[] { nodeName, nodeId };
+                if (structure.Childs != null)
+                {
+                    commonNode.Concat(ClubStructuresToTreeNodes(structure.Childs));
+                }
+
+                result.Add(new TreeNode(structure.Name, commonNode));
+            }
+
+            return result.ToArray();
         }
     }
 }
